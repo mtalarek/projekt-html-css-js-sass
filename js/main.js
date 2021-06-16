7036af46b464d51068ac4852d36e641196d5ccbe
mtalarek <talar_m@wp.pl>
2021-06-16 15:07:35 +0200

    gotowe

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ document.getElementsByClassName('mobile-close')[0].addEventListener('click', fun
     document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
 });
 
-const send = (appointment) => {
-    
+const createAppointment = (appointment) => {
+    const appointmentMessage = document.querySelector('.appointment-message');
+
     fetch('https://akademia108.pl/api/ajax/post-appointment.php', {
         headers: {
             'Content-Type': 'application/json',
@@ -19,130 +20,42 @@ const send = (appointment) => {
     .then(res => res.json())
     .then(resJSON => {
         console.log(resJSON);
+        appointmentMessage.classList.add('send');
+        appointmentMessage.innerText = `Dziękujemy ${resJSON.appointment.name}. Zostałeś zapisany!`
     });
 }
 
-
-let formula = document.getElementById('formula');
-let error = document.getElementById('error');
-let suma = 0;
-
-formula.addEventListener("submit", function(e) {
+document.getElementById('appointment-form').addEventListener("submit", function(e) {
     e.preventDefault();
     
-    
-
-    let namee = formula[0].value;
-    let emaill = formula[1].value;
-    let checkk = formula[2].value;
-    let phonee = formula[3].value;
-    let dataa = formula[4].value;
-    let hourr = formula[5].value;
-    let notess = formula[6].value;
-
-    
-
-
-
+    const appointmentMessage = document.querySelector('.appointment-message');
+    let formFields = document.getElementsByClassName('form-field');
+    let appointmentForm = document.getElementsByClassName('form-field');
+    let allFields = 0;
     let appointment = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        service: document.getElementById('service').value,
-        phone: document.getElementById('phone').value,
-        data: document.getElementById('data').value,
-        hour: document.getElementById('time').value,
-        notes: document.getElementById('notes').value,
+        name: document.getElementById('appointment-name').value,
+        email: document.getElementById('appointment-email').value,
+        service: document.getElementById('appointment-service').value,
+        phone: document.getElementById('appointment-phone').value,
+        date: document.getElementById('appointment-date').value,
+        time: document.getElementById('appointment-time').value,
+        message: document.getElementById('appointment-message').value,
     }
 
-
-    if (namee.trim() == "") {
-        error.classList.add("error");
-        formula[0].classList.add("border");
-        
-    }
-    else {
-        error.classList.remove("error");
-        formula[0].classList.remove("border");
-        
+    for(let i=0; i<appointmentForm.length; i++) {
+        if(appointmentForm[i].value === '') {
+            allFields = allFields + 1;
+            appointmentForm[i].classList.add('error');
+        } else {
+            appointmentForm[i].classList.remove('error');
+        }
     }
 
-
-    if (emaill.trim() == "") {
-        error.classList.add("error");
-        formula[1].classList.add("border"); 
-        
-    } 
-    else {
-        error.classList.remove("error");
-        formula[1].classList.remove("border");
+    if(allFields == 0) {
+        createAppointment(appointment);
+        appointmentMessage.classList.remove('error');
+    } else {
+        appointmentMessage.classList.add('error');
+        appointmentMessage.innerText = 'Wypełnij wymagane pola'
     }
-
-
-    if (checkk !== 'select') {
-        error.classList.remove("error");
-        formula[2].classList.remove("border");
-        
-    }
-    else {
-        error.classList.add("error");
-        formula[2].classList.add("border");
-    }
-
-
-    if (phonee.trim() == "") {
-        error.classList.add("error");
-        formula[3].classList.add("border"); 
-        
-    } 
-    else {
-        error.classList.remove("error");
-        formula[3].classList.remove("border");
-    }
-
-
-    if (dataa.trim() == "") {
-        error.classList.add("error");
-        formula[4].classList.add("border"); 
-        
-    } 
-    else {
-        error.classList.remove("error");
-        formula[4].classList.remove("border");
-    }
-
-
-    if (hourr.trim() == "") {
-        error.classList.add("error");
-        formula[5].classList.add("border"); 
-        
-    } 
-    else {
-        error.classList.remove("error");
-        formula[5].classList.remove("border");
-    }
-
-
-    if (notess.trim() == "") {
-        error.classList.add("error");
-        formula[6].classList.add("border");
-        
-    } 
-    else {
-        error.classList.remove("error");
-        formula[6].classList.remove("border");
-    }
-
-    /* console.log(name);
-    console.log(email);
-    /* console.log(check); */
-    /* console.log(phone);
-    console.log(data);
-    console.log(hour);
-    console.log(notes); */
-
-    if(error.classList[0]!="error") { 
-        send(appointment);
-        
-    }
-
 });
